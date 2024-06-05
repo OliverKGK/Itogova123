86c41e5cc2babe911ee8a15d74cefcc58687f6fd
Arthur <arthur.yusipov@mail.ru>
2024-06-05 13:54:08 +0400

    itog

---
+++ Домашка 6,2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System; 
+ 
+class Program 
+{ 
+    static void Main() 
+    { 
+        int[] myArray = { 1, 2, 3, 4, 5 }; 
+ 
+        Console.WriteLine("Элементы массива, начиная с конца:"); 
+        PrintArrayReverse(myArray, myArray.Length - 1); 
+    } 
+ 
+    static void PrintArrayReverse(int[] arr, int index) 
+    { 
+        if (index >= 0) 
+        { 
+            Console.Write(arr[index] + " "); 
+            PrintArrayReverse(arr, index - 1); 
+        } 
+    } 
+}
---
+++ Домашка 6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System; 
+ 
+class Program 
+{ 
+    static void Main() 
+    { 
+        Console.Write("Введите условие M: "); 
+        int M = int.Parse(Console.ReadLine()); 
+ 
+        Console.Write("Введите условие N: "); 
+        int N = int.Parse(Console.ReadLine()); 
+ 
+        PrintNaturalNumbers(M, N); 
+    } 
+ 
+    static void PrintNaturalNumbers(int current, int N) 
+    { 
+        if (current <= N) 
+        { 
+            Console.Write(current + " "); 
+            PrintNaturalNumbers(current + 1, N); 
+        } 
+    } 
+}
