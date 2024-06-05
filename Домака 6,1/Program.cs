e3df80d41a6321d951c481a9e69c5478cbd53239
Arthur <arthur.yusipov@mail.ru>
2024-06-05 14:32:00 +0400

    itogovaia

--- Домака 6,1/Program.cs
+++ Домака 6,1/Program.cs
@@ -1,26 +1,28 @@
-﻿using System; 
- 
-class Program 
-{ 
-    static void Main() 
-    { 
-        Console.Write("Введите неотрицательное число m: "); 
-        int m = int.Parse(Console.ReadLine()); 
- 
-        Console.Write("Введите неотрицательное число n: "); 
-        int n = int.Parse(Console.ReadLine()); 
- 
-        int result = AckermannFunction(m, n); 
-        Console.WriteLine($"Значение функции Аккермана для ({m}, {n}) равно: {result}"); 
-    } 
- 
-    static int AckermannFunction(int m, int n) 
-    { 
-        if (m == 0) 
-            return n + 1; 
-        else if (n == 0) 
-            return AckermannFunction(m - 1, 1); 
-        else 
-            return AckermannFunction(m - 1, AckermannFunction(m, n - 1)); 
-    } 
+﻿using System;
+
+class Program
+{
+    static int Ackermann(int m, int n)
+    {
+        if (m == 0)
+        {
+            return n + 1;
+        }
+        else if (n == 0)
+        {
+            return Ackermann(m - 1, 1);
+        }
+        else
+        {
+            return Ackermann(m - 1, Ackermann(m, n - 1));
+        }
+    }
+    static void Main()
+    {
+        int m =11;
+        int n = 3;
+
+        int result = Ackermann(n, m);
+        Console.WriteLine($"Ackermann({m}, {n}) = {result}");
+    }
 }
--- Домака 6,1/obj/Debug/net8.0/Домака 6,1.AssemblyInfo.cs
+++ Домака 6,1/obj/Debug/net8.0/Домака 6,1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Домака 6,1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86c41e5cc2babe911ee8a15d74cefcc58687f6fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Домака 6,1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Домака 6,1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
